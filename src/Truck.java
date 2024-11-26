1da31868ccf07d99f63d63eacb587cb2e5600c1e
Sonyaandr <andreevasd8@gmail.com>
2024-11-26 18:56:05 +0300

    добавлены сеттеры и геттеры

--- src/Truck.java
+++ src/Truck.java
@@ -1,6 +1,22 @@
 public class Truck {
     public String modelName;
     public int wheelsCount;
+    public Truck(String modelName, int wheelsCount){
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+    public int getWheelsCount(){
+        return wheelsCount;
+    }
+    public void setWheelsCount(int wheelsCount){
+        this.wheelsCount =wheelsCount;
+    }
+    public String getModelName(){
+        return modelName;
+    }
+    public void setModelName(){
+        this.modelName = modelName;
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
